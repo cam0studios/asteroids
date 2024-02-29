e2a0c3880b6f3fb4cd9230ccc1bc262076ac31e9
Camer0 <90651585+cam0studios@users.noreply.github.com>
2024-02-29 12:53:27 -0500

    add logEvent function

--- firebase.js
+++ firebase.js
@@ -19,3 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+window.log = (name, data) => {
+  logEvent(analytics, name, data);
+}
